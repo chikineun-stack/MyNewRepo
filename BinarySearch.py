76a79f7ba57dc3abbd8c34d8d635061768ea43b0
chikineun <chikineun@gmail.com>
2026-09-17 16:37:27 +0900

    Two files have been modified

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,13 +1,13 @@
 a = [5, 12, 28, 29, 40, 41, 53, 54, 68, 69, 79, 80, 83, 89, 90, 100]
-x = input('Input a number: ')
+x = input('다른내용')
 left = 0
 right = len(a) - 1
 
-# Find x in the list a
+
 while left <= right:
     middle = (left + right) // 2
   
-    if a[middle] == int(x):  # Success
+    if a[middle] == int(x):  
         print('There is {:3} at a[{:2}] in the list.'.format(x, middle))
         break   
     elif a[middle] < int(x):
@@ -15,5 +15,5 @@ while left <= right:
     else:
         right = middle - 1
 
-if left > right: # Fail
-    print('There is not {:3} in this list.'.format(x))
+if left > right: 
+    print('다른내용')
